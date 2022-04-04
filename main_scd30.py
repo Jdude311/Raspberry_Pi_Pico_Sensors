9d58cb08f59a10f5577a75dd9bbf532edf16bfb0
Jdude311 <jadenjohn.ch@gmail.com>
2022-04-04 11:38:59 -0400

    scd30 main codegit

--- main_scd30.py
+++ main_scd30.py
@@ -18,11 +18,10 @@ from digitalio import DigitalInOut
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 from adafruit_io.adafruit_io import IO_MQTT
 
-i2c = busio.I2C(board.GP21, board.GP20, frequency=100000)
+i2c = busio.I2C(board.GP27, board.GP26, frequency=100000)
 
 # Set up sensor
-scd30 = adafruit_scd30.Adafruit_SCD30(i2c)
-print("SGP30 serial #", [hex(i) for i in sgp30.serial])
+scd30 = adafruit_scd30.SCD30(i2c)
 
 # Set up secrets
 try:
@@ -51,39 +50,18 @@ broker = IO_MQTT(mqtt_client)
 broker.connect()
 print("Connected to MQTT!")
 
-# Load saved calibration data
-sgp30.iaq_init()
-base_eCO2_saved = microcontroller.nvm[0:6]
-base_TVOC_saved = microcontroller.nvm[6:12]
-if base_eCO2_saved != bytearray(b'\x00'*6) and base_eCO2_saved != bytearray(b'\xff'*6):
-    if base_TVOC_saved != bytearray(b'\x00'*6) and base_TVOC_saved != bytearray(b'\xff'*6):
-        sgp30.set_iaq_baseline(
-            int(str(base_eCO2_saved)[12:18], 16), # fucking bastard code ikr
-            int(str(base_TVOC_saved)[12:18], 16)  # necessary because i'm stupid and can't get the actual value from bytearray
-        )
-print(
-    "**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
-    % (sgp30.baseline_eCO2, sgp30.baseline_TVOC)
-)
-del base_eCO2_saved, base_TVOC_saved
-
-# Saves baseline to NVM
-def save_baseline():
-    base = sgp30.get_iaq_baseline() # eCO2, TVOC
-    baseline_eCO2, baseline_TVOC = base[0], base[1]
-    microcontroller.nvm[0:6] = bytearray(hex(baseline_eCO2))
-    microcontroller.nvm[6:12] = bytearray(hex(baseline_TVOC))
-    print(
-        "**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
-        % (sgp30.baseline_eCO2, sgp30.baseline_TVOC)
-    )
-
 # Takes reading and saves/prints/sends
 def take_reading():
-    iaq = sgp30.iaq_measure()
-    eCO2, TVOC = iaq[0], iaq[1] # eCO2, TVOC
-    eCO2_reads.append(eCO2)
+    iaq = [
+        scd30.CO2,
+        scd30.temperature,
+        scd30.relative_humidity
+    ]
+    CO2, TVOC, temperature, relative_humidity = iaq[0], None, iaq[1], iaq[2] # eCO2, TVOC
+    CO2_reads.append(CO2)
     TVOC_reads.append(TVOC)
+    temperature_reads.append(temperature)
+    relative_humidity_reads.append(relative_humidity)
 
 # Evaluates median. Utility function
 def median(l):
@@ -96,48 +74,39 @@ def median(l):
 
 # Sends readings
 def send_reading():
-    eCO2, TVOC = median(eCO2_reads), median(TVOC_reads)
-    location = "Jaden\'s Room"
+    CO2, TVOC, temperature, relative_humidity = median(CO2_reads), None, median(temperature_reads), median(relative_humidity_reads)
+    location = "Jaden\'s Room (SCD30)"
     topic = "test"
-    msg = f"{location},{TVOC},{eCO2}"
+    msg = f"{location},0,{CO2},{temperature},{relative_humidity}"
     broker.publish(topic, msg)
-    print("*** PUBLISHED: eCO2: %d ppm \t TVOC: %d ppb" % (eCO2, TVOC))
+    print(msg)
 
 
 
 # Warm up sensor
-for i in range(20):
+while True:
     time.sleep(1)
-    eCO2 = sgp30.eCO2
-    TVOC = sgp30.TVOC
+    if scd30.data_available:
+        CO2 = scd30.CO2
+        break
 
 # Main loop
 read_sec = 0
 base_sec = 0
-eCO2_reads, TVOC_reads = [], []
+CO2_reads, TVOC_reads, temperature_reads, relative_humidity_reads = [], [], [], []
 while True:
     time.sleep(1)
     read_sec += 1
     base_sec += 1
     try:
         # Take reading every 30 seconds
-        if read_sec >= 30:
+        if read_sec >= 10:
             send_reading()
-            eCO2_reads, TVOC_reads = [], []
+            CO2_reads, TVOC_reads, temperature_reads, relative_humidity_reads = [], [], [], []
             read_sec = 0
-        else:
+        elif scd30.data_available:
             take_reading()
-            print("eCO2: %d ppm \t TVOC: %d ppb" % (eCO2_reads[-1], TVOC_reads[-1]))
-
-        if base_sec % 10 == 0:
-            base = sgp30.get_iaq_baseline() # eCO2, TVOC
-            baseline_eCO2, baseline_TVOC = base[0], base[1]
-            print("*** Baseline: " + str(baseline_eCO2) + "\t" + str(baseline_TVOC))
-
-        # Save baseline hourly
-        if base_sec >= 3600:
-            save_baseline()
-            base_sec = 0
+            #print("CO2: %d ppm \t TVOC: %d ppb" % (CO2_reads[-1], TVOC_reads[-1]))
 
     except (ValueError, RuntimeError) as e:
         print("Failed. Retrying.")
